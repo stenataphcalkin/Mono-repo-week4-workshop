b8cd25e91b9758e2141dcc84f2ffce21ee6132cf
Steph <steph@polyhydral.co.uk>
2025-09-01 16:40:36 +0100

    attempt2

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 async function getMessage() {
   const response = await fetch(
-    "https://mono-repo-client-2kqz.onrender.com/message"
+    "https://mono-repo-week4-workshop.onrender.com/message"
   );
 
   const message = await response.json();
